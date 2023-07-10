015c4fc054d366ddeb3e7e8d25ff70eb95bdb86c
g18s3628 <36521304+g18s3628@users.noreply.github.com>
2023-07-10 22:27:18 +0200

    details.py

--- details.py
+++ details.py
@@ -1,1 +1,11 @@
-e# details.py
+#Request user to input their name using input() command 
+#Request user to input their age using input() command
+#Request user to input their house number using input() command 
+#Request user to input their street name using input() command
+#Print out the inputs in a sentence using the f-string and including the responses
+
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+house_num = int(input("Enter your house number:"))
+street_name = input("Enter your street name:")
+print(f"This is {name}. He/She is {age} and lives at house number {house_num} at {street_name}")
